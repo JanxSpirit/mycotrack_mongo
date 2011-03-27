0c47043bbe0317ef971f7b1766904286c86481ce
Gregg Carrier <greggcarrier@gmail.com>
2011-03-27 08:42:35 -0700

    species list link to edit species form

--- src/main/scala/com/mycotrack/snippet/ManageSpecies.scala
+++ src/main/scala/com/mycotrack/snippet/ManageSpecies.scala
@@ -5,17 +5,27 @@
  */
 package com.mycotrack.snippet
 
-import xml.NodeSeq
+import xml.{NodeSeq, Text}
 import _root_.net.liftweb.util.Helpers
 import Helpers._
 import net.liftweb.http.{RequestVar, S, TemplateFinder, SHtml}
 import com.mycotrack.model.{Species, User}
+import org.bson.types.ObjectId
 import net.liftweb.common.Empty
+import net.liftweb.common.Box._
 
 class ManageSpecies {
 
   def add(xhtml: NodeSeq): NodeSeq = {
-    val species = Species.createRecord
+    //val species = Species.createRecord
+
+    val speciesb = for {
+      id <- S.param("id")
+      s <- Species.find(new ObjectId(id))
+    } yield s
+
+    val species = speciesb.getOrElse(Species.createRecord)
+
     var scientificName = species.scientificName.is
     var commonName = species.commonName.is
     var infoUrl = species.infoUrl.is
@@ -31,5 +41,16 @@ class ManageSpecies {
         species.save
         S.redirectTo("/manageSpecies")
       }))
+
+  }
+
+  def list(xhtml: NodeSeq): NodeSeq = {
+    Helpers.bind("species", xhtml,
+      "allSpecies" -> {
+        for (species <- Species.findAll) yield <li>
+          {SHtml.link("/manageSpecies?id=" + species._id.toString, () => Unit, Text(species.commonName.get))}
+        </li>
+      }
+    )
   }
 }
